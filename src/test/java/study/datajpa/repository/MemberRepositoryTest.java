6e50d949dfefe894a703157114d00c76b5d8f48f
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2023-08-10 20:46:13 +0900

    프로젝트 생성 SpringData JPA 예제 활용 JPA - > SPRING DATA JPA 활용 방법
    interface 생성 후 extends JpaRepository<타입, 아이디 타입>을 상속받아
    해당 인터페이스에는 crud paging 간단 조회 boolean 체크 count 체크등을 조회 할 수 있는 메소드들이 정의 되어 있는 interface를 통해 일반 JPA와 다르게 해당 내용들의 DB 쿼리 조회의 방법들을 해당 JpaRepository 상속 받아 그위에 공통으로 있는 crud 나 paging 공통 common
    DB접근 기술이 바뀌어도 활용 가능한 springData interface를 상속한 Jpa 특성의 JpaRepository interface를 상속 하면 spring 내부에서 우리가 구현체를 만드는게 아니라 해당 정의되어있는 interface에 맞춘 구현체를 spring에서 proxy객체를 만들어 구현체를 자동으로 주입하여서 우리는 해당 정의된 메소드명을 사용하는 것으로 db접근 기술을 쉽게 사용 할 수 있다.
    
    @NamedQuery class 설정 방법 (실무에서 잘 사용 안함)
    @Query 메소드에 쿼리 직접 작성
    @Query named query 연결
    @Query로 new 오퍼레이션을 통해 Dto값 조회
    파라미터 ?index :name  순차별로 파라미터 조회하는 것은 실무 사용 x
    파라미터값은 @Param으로 매개변수를 통해 사용
    변환 타입 여러개로 설정 가능 object, collection, Optional등 반환 가능
    주의 사항으로는 object는 null이 나옴 jpa와 다르게 spring data jpa는
    예외처리를 통해 null 오류를 변환해서 출력 함
    
    페이징과 정렬 파라미터
    org.springframework.data.domain.Sort : 정렬 기능
    org.springframework.data.domain.Pageable : 페이징 기능 (내부에 Sort 포함)
    
    특별한 반환 타입
    org.springframework.data.domain.Page : 추가 count 쿼리 결과를 포함하는 페이징
    org.springframework.data.domain.Slice : 추가 count 쿼리 없이 다음 페이지만 확인 가능(내부적
    으로 limit + 1조회)
    List (자바 컬렉션): 추가 count 쿼리 없이 결과만 반환
    
    주의: Page는 1부터 시작이 아니라 0부터 시작이다.

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -4,10 +4,17 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,10 +27,13 @@ import static org.junit.jupiter.api.Assertions.*;
 @Rollback(false)
 class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
-    public void testMember(){
+    public void testMember() {
         System.out.println("memberRepository.getClass() = " + memberRepository.getClass());
         Member member = new Member("memberA");
         Member savedMember = memberRepository.save(member);
@@ -38,7 +48,7 @@ class MemberRepositoryTest {
     }
 
     @Test
-    public void basicCRUD(){
+    public void basicCRUD() {
         Member member1 = new Member("member1");
         Member member2 = new Member("member2");
 
@@ -66,6 +76,143 @@ class MemberRepositoryTest {
 
         long deleteCount = memberRepository.count();
         assertThat(deleteCount).isEqualTo(0);
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThen() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void findHelloBy() {
+        List<Member> helloBy = memberRepository.findTop3HelloBy();
+    }
+
+    @Test
+    public void testNamedQuery() {
+
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsername("AAA");
+        Member findMember = result.get(0);
+        assertThat(findMember).isEqualTo(m1);
+
+    }
+
+
+    @Test
+    public void testQuery() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+        assertThat(result.get(0)).isEqualTo(m1);
+    }
+
+    @Test
+    public void findUsernameList() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> usernameList = memberRepository.findUsernameList();
+        for (String s : usernameList) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void findMemberDto() {
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member m1 = new Member("AAA", 10);
+        m1.setTeam(team);
+        memberRepository.save(m1);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+        for (MemberDto MemberDtos : memberDto) {
+            System.out.println("MemberDtos = " + MemberDtos);
+        }
+    }
+
+    @Test
+    public void findByNames() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        for (Member member : result) {
+            System.out.println("member = " + member);
+        }
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        //List<Member> aaa = memberRepository.findListByUsername("AAA");
+        //Member aaa = memberRepository.findMemberByUsername("AAA");
+
+        Optional<Member> aaa = memberRepository.findOptionByUsername("AAA");
+        System.out.println("aaa = " + aaa);
+    }
+
+    @Test
+    public void paging(){
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 10));
+        memberRepository.save(new Member("member3", 10));
+        memberRepository.save(new Member("member4", 10));
+        memberRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+        //when
+        Page<Member> page = memberRepository.findByAge(age, pageRequest);
+        //Slice<Member> page = memberRepository.findSliceByAge(age, pageRequest);
+
+        Page<MemberDto> toMap = page.map(m -> new MemberDto(m.getId(), m.getUsername(), null));
+
+
+        System.out.println("----------page--------- = " + page);
+        //then
+        List<Member> content = page.getContent();
+        //long totalElements = page.getTotalElements();
+
+        for (Member member : content) {
+            System.out.println("member = " + member);
+        }
+        //System.out.println("totalElements = " + totalElements);
+
+        assertThat(content.size()).isEqualTo(3);
+        assertThat(page.getTotalElements()).isEqualTo(5);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getTotalPages()).isEqualTo(2);
+        assertThat(page.isFirst()).isTrue();
+        assertThat(page.hasNext()).isTrue();
 
     }
 
